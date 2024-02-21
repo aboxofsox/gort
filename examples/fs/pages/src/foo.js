eaf67c320a9ee7606500997e90bf2a421f07220a
aboxofsox <idkwthfml@gmail.com>
2024-02-21 01:35:38 -0600

    🛠️ static paths

---
+++ examples/fs/pages/src/foo.js
@@ -0,0 +1,3 @@
+export function doThing() {
+    console.log('doing thing...');
+}
--- examples/fs/pages/src/index.js
+++ examples/fs/pages/src/index.js
@@ -1,5 +1,8 @@
+import {doThing} from './foo.js';
+
 const h1 = document.querySelector('h1');
 const helloBtn = document.querySelector('#helloBtn');
 helloBtn.addEventListener('click', () => {
     h1.textContent = 'Goodbye'
 });
+doThing();
